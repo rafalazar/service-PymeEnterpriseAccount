9b49a09b3e3a9126c7e57d924480bf32c3bfa32e
Jordan <jcr.salazar@outlook.com>
2020-01-22 14:52:16 -0500

    Microservicio creado.

---
+++ src/main/java/com/rafalazar/bootcamp/app/ServicePymeEnterpriseAccountApplication.java
@@ -0,0 +1,13 @@
+package com.rafalazar.bootcamp.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicePymeEnterpriseAccountApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicePymeEnterpriseAccountApplication.class, args);
+	}
+
+}
